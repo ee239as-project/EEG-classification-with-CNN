99dfcbc3d53724389624d7125183ddefe2fb6b7a
Eric Fischer <ericf712@gmail.com>
2019-03-14 14:20:33 -0700

    updates LSTM.py

--- Utils/visualize.py
+++ Utils/visualize.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
 
+
 def get_accuracy(ouput, target, batch_size):
-    ''' Obtain accuracy for training round '''
+    # get accuracy for training batch
     classes_predicted = torch.max(ouput, 1)[1]
-    corrects = (np.equal(classes_predicted.tolist(),target.tolist()).astype(int)).sum()
-    # corrects = (max_values[1].view(target.size()).data == target.data).sum()
-    accuracy = 100.0 * corrects/batch_size
+    correct = (np.equal(classes_predicted.tolist(), target.tolist()).astype(int)).sum()
+    # correct = (max_values[1].view(target.size()).data == target.data).sum()
+    accuracy = 100.0 * correct / batch_size
     return accuracy.item()
