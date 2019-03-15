13acbf9f8be9a2b28ac6948f1e5101bdd5a09e83
Eric Fischer <ericf712@gmail.com>
2019-03-14 18:35:42 -0700

    creates CNN_v2.py

---
+++ Models/CNN_v2.py
@@ -0,0 +1,152 @@
+# Fix for issue loading Utils.preprocess_util
+import os, sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+# Variable: wrapper for PyTorch tensor, stores gradients with requires_grad=True
+# x.data, x.grad.data: values of tensor x, gradient values of x
+from torch.autograd import Variable
+from Utils.preprocess_util import *
+from Utils.visualize import *
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        print('Flatten forward x.size:', x.size())
+        a = x.view(x.size(0), -1)
+        return a
+
+class threed_to_twod(nn.Module):
+    def forward(self, x):
+        print('threed_to_twod x.shape:', x.shape)
+        a = x.reshape(x.shape[0], x.shape[3], x.shape[1])
+        print ('a.shape: ', a.shape)
+        return a
+
+X_train,X_valid,X_test, Y_train,Y_valid,Y_test = load_preprocess_eeg_data()
+
+# CPU datatype: change to torch.cuda.FloatTensor to use GPU
+dtype = torch.FloatTensor
+
+# N, C, H, W = 18, 1, 25, 1000
+# x = Variable(torch.tensor(X_train.reshape((18, 1, 25, 1000))))
+x = Variable(torch.tensor(X_train))
+y = Variable(torch.tensor(Y_train), requires_grad=False)
+x.type(dtype)
+y.type(dtype)
+
+# ------------------------------------- CNN MODEL -------------------------------------
+dropout = 0
+model = nn.Sequential()
+model.add_module('conv_across_time', nn.Conv2d(1, 40, kernel_size=(1,51) ,stride=1))
+model.add_module('conv_across_electrodes', nn.Conv2d(40, 40, kernel_size=(22,1), stride=1))
+model.add_module('BatchNorm2d', nn.BatchNorm2d(40, momentum=0.1))
+model.add_module('Nonlinearity', nn.ReLU(inplace=True))
+model.add_module('correct_dimensions', threed_to_twod())
+model.add_module('AvgPool2d', nn.AvgPool2d(kernel_size=(135,1), stride=(5,1)))
+model.add_module('drop', nn.Dropout(p=dropout))
+model.add_module('Flatten', Flatten())
+model.add_module('Fc_layer', nn.Linear(2560,10))
+torch.nn.init.xavier_uniform_(model.conv_across_time.weight, gain=1)
+torch.nn.init.xavier_uniform_(model.conv_across_electrodes.weight, gain=1)
+
+model.type(dtype)
+loss_fn = nn.CrossEntropyLoss().type(dtype)
+
+# ---------------------------------- HYPERPARAMETERS ----------------------------------
+lr = 0.0001
+betas = (0.9, 0.999)
+eps = 1e-08
+wt_dcy = 0
+amsgrad = False
+params = model.parameters()
+optimizer = optim.Adam(params, lr=lr, betas=betas, eps=eps, weight_decay=wt_dcy, amsgrad=amsgrad)
+
+# ------------------------------------- TRAINING -------------------------------------
+'''
+for t in range(3):
+    # calculate loss
+    y_pred = model(x.float())
+    loss = loss_fn(y_pred, y.type(torch.LongTensor))
+
+    # backprop
+    model.zero_grad()
+    loss.backward()
+
+    # update the parameters
+    optimizer.step()
+y_pred = model(x)
+loss = loss_fn(y_pred, y.type(torch.LongTensor))
+print(loss)
+print(model)
+'''
+n_train = X_train.shape[0]
+# n_valid = X_valid.shape[0]
+batch_size = 100
+num_epochs = 50
+iterations_per_epoch = max(n_train // batch_size, 1)
+num_iterations = num_epochs * iterations_per_epoch
+epoch = 1
+
+train_loss = []
+iterations = []
+for t in range(num_iterations):
+    batch_mask = np.random.choice(n_train, batch_size)
+    X_batch = X_train[batch_mask]
+    y_batch = Y_train[batch_mask]
+    X_batch_tensor = threeD_to_fourDTensor(X_batch)
+    y_batch_tensor = Variable(torch.tensor(y_batch))
+
+    y_pred = model(X_batch_tensor.float())
+
+    loss = loss_fn(y_pred, y_batch_tensor.type(torch.LongTensor))
+
+    train_loss.append(loss.data)
+    iterations.append(t)
+
+    model.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if (t%10 == 0):
+        print('(Iteration %d / %d) loss: %f' % (t + 1, num_iterations,
+                                                loss.detach().numpy()))
+
+    epoch_end = (t + 1) % iterations_per_epoch == 0
+
+    if epoch_end:
+        epoch += 1
+
+    first_it = (t == 0)
+    last_it = (t == num_iterations - 1)
+
+    if first_it or last_it or epoch_end:
+        X_train_tensor = threeD_to_fourDTensor(X_train[0:100,:,:])
+        y_pred_train = model(X_train_tensor.float())
+        train_acc = get_accuracy(y_pred_train, Y_train[0:100], batch_size=50)
+
+        X_valid_tensor = threeD_to_fourDTensor(X_valid[0:50,:,:])
+        y_pred_valid = model( X_valid_tensor.float())
+        val_acc = get_accuracy(y_pred_valid, Y_valid[0:50], batch_size=50)
+        print('(Epoch %d / %d) train acc: %f; val_acc: %f' % (epoch, num_epochs,
+                                                              train_acc, val_acc))
+
+
+# ------------------------------------- ACCURACY -------------------------------------
+# Validation accuracy
+get_accuracy_in_batches(model, X_valid, Y_valid, seq_dim, input_dim, 'validation',
+                        iterations=500, cnn=True)
+# Testing accuracy
+get_accuracy_in_batches(model, X_test, Y_test, seq_dim, input_dim, 'testing',
+                        iterations=500, cnn=True)
+# Training accuracy
+get_accuracy_in_batches(model, X_train, Y_train, seq_dim, input_dim, 'training',
+                        iterations=500, cnn=True)
+
+plt.plot(iterations, train_loss)
+plt.show()
